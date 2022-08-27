d3ca7b27d04d64d249dcc24318b7ebf1601569e9
uhorn <uwehornnet@users.noreply.github.com>
2022-08-27 15:07:29 +0200

    changes: fetch request based on url in scan result

--- src/context/index.js
+++ src/context/index.js
@@ -15,25 +15,24 @@ export const ContextProvider = ({ children }) => {
 	const [showReader, setShowReader] = useState(false);
 
 	const [result, setResult] = useState(null);
+	const [output, setOutput] = useState(null);
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(null);
 
 	const fetchDataAsync = async () => {
 		try {
 			if (!loading) setLoading(true);
-			const req = await fetch(`${endpoint}/api/`, {
-				method: "POST",
+			const req = await fetch(result, {
+				method: "GET",
 				headers: {
 					"Content-Type": "application/json",
 				},
-				body: JSON.stringify({
-					id: result,
-				}),
 			});
 			const res = await req.json();
 			// do something with the result
 
 			console.log({ res });
+			setOutput(res);
 			setLoading(false);
 		} catch (err) {
 			setError(err);
@@ -50,6 +49,7 @@ export const ContextProvider = ({ children }) => {
 				result,
 				setResult,
 				CONFIG,
+				output,
 				showReader,
 				setShowReader,
 				fetchDataAsync,
